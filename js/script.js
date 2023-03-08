5e1778264bf18e67b1793c87963970624b9b38b0
Maureen_g <gamradicalsix@gmail.com>
2023-03-08 14:58:13 -0600

    Initial commit, linking html and js pages

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//console.log("Hi! JS and HTML are connected!");
+
+var mainTitle = document.querySelector("h1");
+console.log(mainTitle);
+//<h1>JavaScript, our Magical Friend ✨</h1>
+
+mainTitle.innerText = "Yay, JavaScript!!";
+//<h1>Yay, JavaScript!!</h1>
+
+var body = document.querySelector("body");
+console.log(body);
+//<body>…</body>
+
+body.style.backgroundColor = "#FEF2F1";
+//<body style="background: rgb(255, 238, 255);">…</body>
